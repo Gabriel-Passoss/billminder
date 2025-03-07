92ed318729e2ef52fcc9364ef5ece91b52fd4738
Gabriel Passos <contato.gabrielpassoss@gmail.com>
2025-03-07 17:44:22 -0300

    Initial commit with base project

---
+++ BillMinder/BillMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillMinderApp.swift
+//  BillMinder
+//
+//  Created by Gabriel dos Passos on 07/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BillMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BillMinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BillMinder
+//
+//  Created by Gabriel dos Passos on 07/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
