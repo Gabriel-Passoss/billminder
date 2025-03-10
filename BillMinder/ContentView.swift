5f624b2941733eae3fb45636ad789dd668793b70
Gabriel Passos <contato.gabrielpassoss@gmail.com>
2025-03-10 14:35:51 -0300

    add: bottom tab navigation

--- BillMinder/ContentView.swift
+++ BillMinder/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            VStack {
+                
+            }
+            .tabItem {
+                Image(systemName: "chart.pie")
+                Text("Metrics")
+            }
+            
+            SubscriptionsView(subscriptions: Subscription.sample)
+                .tabItem {
+                    Image(systemName: "creditcard")
+                    Text("Subscriptions")
+                }
         }
-        .padding()
     }
 }
 
